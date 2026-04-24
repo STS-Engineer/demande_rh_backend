0764e138d8f5158afd9ec069b22a0cbc55486933
STS-Engineer <administration.sts@avocarbon.com>
2026-04-24 12:01:28 +0100

    Change cron job schedule to run at 12:03 PM

--- server.js
+++ server.js
@@ -1973,7 +1973,7 @@ app.get('/api/smtp-status', async (req, res) => {
  try {
    const cron = require('node-cron');
 
-  cron.schedule('35 11 * * 1-5', async () => {
+  cron.schedule('03 12 * * 1-5', async () => {
      console.log("⏰ Running automatic attendance reports...");
      await sendAttendanceReport();
      //await sendTeamAttendanceReportPerResponsable();
