db741e2ca47d372742241493f6b9ac3ee5a9b494
STS-Engineer <administration.sts@avocarbon.com>
2026-05-11 08:35:52 +0100

    Modify attendance report scheduling
    Commented out the await call for sendAttendanceReport.

--- server.js
+++ server.js
@@ -1990,7 +1990,7 @@ try {
 
   cron.schedule('30 9 * * 1-5', async () => {
     console.log("⏰ Running automatic attendance reports...");
-    await sendAttendanceReport();
+    //wait sendAttendanceReport();
     //await sendTeamAttendanceReportPerResponsable();
   }, { timezone: "Africa/Tunis" });
 
